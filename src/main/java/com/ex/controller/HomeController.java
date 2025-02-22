5e0e799b78bd297b0d1cdca90ddb96ac96e57423
Selene Munoz <smunozgo@my.centennialcollege.ca>
2025-02-21 22:03:58 -0500

    Update HomeController.java

--- src/main/java/com/ex/controller/HomeController.java
+++ src/main/java/com/ex/controller/HomeController.java
@@ -7,7 +7,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
 
     @GetMapping("/")
+<<<<<<< Updated upstream
     public String welcomeMessage() {
         return "Welcome to COMP367";
+=======
+    public String dynamicGreeting() {
+        LocalTime now = LocalTime.now();
+        String greeting = now.getHour() < 12 ? "Good morning" : "Good afternoon";
+        return greeting + ", Selene, Welcome to COMP367";
+>>>>>>> Stashed changes
     }
 }
